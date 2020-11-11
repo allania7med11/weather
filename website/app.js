e4902b25b46183ff684d95351d2f5f64a1a25955
ahmedallani <ahmed.allani@rbk.tn>
2020-11-11 18:19:59 +0100

    post to server

--- website/app.js
+++ website/app.js
@@ -1,7 +1,9 @@
+console.log("fdhdfsghd")
 let api =
   "http://api.openweathermap.org/data/2.5/weather?appid=ca1d4e22912cae57f8b984655e8a38ba";
+let server = "http://localhost:3000"
 let zip = "94040,us";
-const test = async () => {
+const getData = async () => {
   try {
     let response = await fetch(`${api}&zip=${zip}`, {
       method: "GET",
@@ -12,4 +14,21 @@ const test = async () => {
     console.log({ err });
   }
 };
-test();
+const getFakeData = () => {
+  return window.data;
+};
+let generate = document.getElementById("generate");
+generate.addEventListener("click",async function() {
+  let data = getFakeData();
+  console.log({data});
+  let info={temperature:data.main.temp, date:data.dt, userResponse:zip}
+  let response = await fetch(server + "/data", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(info)
+  });
+  let result = await response.json();
+  console.log({result})
+});
