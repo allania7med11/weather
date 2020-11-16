1b31aca4f299ec4c618a73136728705c2b9d3bc7
ahmedallani <ahmed.allani@rbk.tn>
2020-11-16 20:39:18 +0100

    min

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-let projectData = [{ name: "w1" }, { name: "w2" }];
+let projectData = [];
 app.get("/data", function(req, res) {
   res.send({ data: projectData });
 });
 app.post("/data", function(req, res) {
   const { temperature, date, userResponse } = req.body;
   projectData.push({ temperature, date, userResponse });
-  res.send({ data: projectData });
+  res.send({ success: true });
 });
 
 app.listen(3000, function() {
--- website/app.js
+++ website/app.js
@@ -1,15 +1,22 @@
-console.log("fdhdfsghd")
+console.log("fdhdfsghd");
+let zip = document.getElementById("zip");
+let results = document.getElementById("results");
 let api =
   "http://api.openweathermap.org/data/2.5/weather?appid=ca1d4e22912cae57f8b984655e8a38ba";
-let server = "http://localhost:3000"
-let zip = "94040,us";
-const getData = async () => {
+let server = "http://localhost:3000";
+let show = [
+  { display: "Temperature", field: "temperature" },
+  { display: "Date", field: "date" },
+  { display: "Zip Code", field: "userResponse" },
+];
+// let zip = "94040,us";
+const getData = async (zip) => {
   try {
     let response = await fetch(`${api}&zip=${zip}`, {
       method: "GET",
     });
     let data = await response.json();
-    console.log({ data });
+    return data;
   } catch (err) {
     console.log({ err });
   }
@@ -18,17 +25,35 @@ const getFakeData = () => {
   return window.data;
 };
 let generate = document.getElementById("generate");
-generate.addEventListener("click",async function() {
-  let data = getFakeData();
-  console.log({data});
-  let info={temperature:data.main.temp, date:data.dt, userResponse:zip}
+generate.addEventListener("click", async function() {
+  console.log(zip.value);
+  let data = await getData(zip.value);
+  console.log({ data });
+  let info = { temperature: data.main.temp, date: data.dt, userResponse: zip.value };
   let response = await fetch(server + "/data", {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json;charset=utf-8'
+      "Content-Type": "application/json;charset=utf-8",
     },
-    body: JSON.stringify(info)
+    body: JSON.stringify(info),
   });
   let result = await response.json();
-  console.log({result})
+  if (result.success) {
+    let response2 = await fetch(server + "/data", {
+      method: "GET",
+    });
+    let data2 = await response2.json();
+    let weather = data2.data;
+    debugger;
+    let last = weather[weather.length - 1];
+    let htmlResults = show.reduce((acc, cv) => {
+      acc += `
+      <div>
+        <span>${cv.display}:</span>
+        <span>${last[cv.field]}</span>
+      </div>`;
+      return acc;
+    },"");
+    results.innerHTML = htmlResults;
+  }
 });
